d261a478d7623cb933888a43fc321258c361184a
Kaustubh Kulkarni <kulkarnikaustubhv@gmail.com>
2021-10-22 22:57:31 +0200

    Added missing readdb method in mainwindow.cs

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -74,6 +74,7 @@ namespace ContactApp
             {
                 ContactDetailWindow contactDetailWindow = new ContactDetailWindow(selectedContact);
                 contactDetailWindow.ShowDialog();
+                ReadDb();
             }
         }
     }
