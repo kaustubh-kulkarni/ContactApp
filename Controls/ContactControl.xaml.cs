97648e3e9bf7c5b6b20e2f2f534f22c05352b7cc
Kaustubh Kulkarni <kulkarnikaustubhv@gmail.com>
2021-10-23 14:14:58 +0200

    Added dependency prop and functionality

--- Controls/ContactControl.xaml.cs
+++ Controls/ContactControl.xaml.cs
@@ -19,18 +19,29 @@ namespace ContactApp.Controls
     /// </summary>
     public partial class ContactControl : UserControl
     {
-        private Contact contact;
-
+        //Dependency prop for contact
         public Contact Contact
         {
-            get { return contact; }
-            set 
-            { 
-                contact = value;
-                nameTextBlock.Text = contact.Name;
-                emailTextBlock.Text = contact.Email;
-                phoneTextBlock.Text = contact.Phone;
+            get { return (Contact)GetValue(ContactProperty); }
+            set { SetValue(ContactProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for Contact.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty ContactProperty =
+            DependencyProperty.Register("Contact", typeof(Contact), typeof(ContactControl), new PropertyMetadata(null, SetText));
+
+        private static void SetText(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            //Get contract control to control and assign it to dependency d
+            ContactControl control = d as ContactControl;
+            if(control != null)
+            {
+                //Get the value as Contact and access name
+                control.nameTextBlock.Text = (e.NewValue as Contact).Name;
+                control.emailTextBlock.Text = (e.NewValue as Contact).Email;
+                control.phoneTextBlock.Text = (e.NewValue as Contact).Phone;
             }
+         
         }
 
         public ContactControl()
