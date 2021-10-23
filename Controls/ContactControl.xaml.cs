33c51ac1b085c6f41ee1f597785a5760d829137d
Kaustubh Kulkarni <kulkarnikaustubhv@gmail.com>
2021-10-23 13:19:22 +0200

    Created custom control

---
+++ Controls/ContactControl.xaml.cs
@@ -0,0 +1,41 @@
+﻿using ContactApp.Classes;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ContactApp.Controls
+{
+    /// <summary>
+    /// Interaction logic for ContactControl.xaml
+    /// </summary>
+    public partial class ContactControl : UserControl
+    {
+        private Contact contact;
+
+        public Contact Contact
+        {
+            get { return contact; }
+            set 
+            { 
+                contact = value;
+                nameTextBlock.Text = contact.Name;
+                emailTextBlock.Text = contact.Email;
+                phoneTextBlock.Text = contact.Phone;
+            }
+        }
+
+        public ContactControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
